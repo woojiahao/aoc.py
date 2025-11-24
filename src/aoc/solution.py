f0d4dc8b17f3e3ad577c3718d09c0a5beddbe544
Jiahao, Woo <woojiahao1234@gmail.com>
2025-11-24 15:53:34 +0800

    Setup aoc

---
+++ src/aoc/solution.py
@@ -0,0 +1,39 @@
+import abc
+from dataclasses import dataclass
+from typing import Any, Literal, Union
+
+TYPE = Union[Literal["live", "test"]]
+
+
+@dataclass
+class Solution(abc.ABC):
+    year: int
+    day: int
+
+    def load_data(self, type: TYPE) -> str:
+        suffix = "-test" if type == "test" else ""
+        with open(f"data/y{self.year}/day{self.day}{suffix}.txt", "r") as f:
+            contents = f.read().strip()
+        return contents
+
+    @abc.abstractmethod
+    def parse_data(self, contents: str) -> Any:
+        pass
+
+    @abc.abstractmethod
+    def solve_one(self, data: Any) -> Any:
+        pass
+
+    @abc.abstractmethod
+    def solve_two(self, data: Any) -> Any:
+        pass
+
+    def part_one(self, type: TYPE) -> Any:
+        data = self.load_data(type)
+        parsed_data = self.parse_data(data)
+        return self.solve_one(parsed_data)
+
+    def part_two(self, type: TYPE) -> Any:
+        data = self.load_data(type)
+        parsed_data = self.parse_data(data)
+        return self.solve_two(parsed_data)
