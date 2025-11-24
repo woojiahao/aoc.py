b23ede7169a04726fc3dafec5cdb2d898dd0f904
Jiahao, Woo <woojiahao1234@gmail.com>
2025-11-24 16:19:52 +0800

    Add Y2024

---
+++ src/aoc/y2024/day13.py
@@ -0,0 +1,17 @@
+
+from typing import Any, List, Tuple
+
+from ..solution import Solution
+
+
+class Y2024Day13(Solution):
+    def parse_data(self, contents: str) -> Any:
+        for line in contents.strip().split("\n"):
+            pass
+        return
+
+    def solve_one(self, data: Any) -> Any:
+        pass
+
+    def solve_two(self, data: Any) -> Any:
+        pass
