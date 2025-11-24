f1d3e9103885ffdcf6a54593a18e1cb2ffdd26b6
Jiahao, Woo <woojiahao1234@gmail.com>
2025-11-24 20:00:20 +0800

    Add Y2024D7

--- src/aoc/y2024/day7.py
+++ src/aoc/y2024/day7.py
@@ -1,4 +1,3 @@
-
 from typing import Any, List, Tuple
 
 from ..solution import Solution
@@ -6,12 +5,43 @@ from ..solution import Solution
 
 class Y2024Day7(Solution):
     def parse_data(self, contents: str) -> Any:
+        ans = []
         for line in contents.strip().split("\n"):
-            pass
-        return
+            [test_value, values] = line.split(": ")
+            int_values = [int(v) for v in values.split(" ")]
+            ans.append((int(test_value), int_values))
+        return ans
 
-    def solve_one(self, data: Any) -> Any:
-        pass
+    def solve_one(self, data: List[Tuple[int, List[int]]]) -> Any:
+        ans = 0
+        for test_value, int_values in data:
+            if self.__can_equate__(test_value, int_values):
+                ans += test_value
+        return ans
 
     def solve_two(self, data: Any) -> Any:
-        pass
+        ans = 0
+        for test_value, int_values in data:
+            if self.__can_equate_2__(test_value, int_values):
+                ans += test_value
+        return ans
+
+    def __can_equate__(self, test_value: int, int_values: List[int]) -> bool:
+        def dfs(i: int, acc: int) -> bool:
+            if i >= len(int_values):
+                return acc == test_value
+            return dfs(i + 1, acc * int_values[i]) or dfs(i + 1, acc + int_values[i])
+
+        return dfs(1, int_values[0])
+
+    def __can_equate_2__(self, test_value: int, int_values: List[int]) -> bool:
+        def dfs(i: int, acc: int) -> bool:
+            if i >= len(int_values):
+                return acc == test_value
+            return (
+                dfs(i + 1, acc * int_values[i])
+                or dfs(i + 1, acc + int_values[i])
+                or dfs(i + 1, int(str(acc) + str(int_values[i])))
+            )
+
+        return dfs(1, int_values[0])
