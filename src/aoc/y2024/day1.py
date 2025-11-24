cdb3b5b38422bd5e25bb502766de870548a90846
Jiahao, Woo <woojiahao1234@gmail.com>
2025-11-24 15:59:09 +0800

    Solve Y2024D1

--- src/aoc/y2024/day1.py
+++ src/aoc/y2024/day1.py
@@ -1,4 +1,5 @@
 from typing import Any, List, Tuple
+from collections import Counter
 
 from ..solution import Solution
 
@@ -10,16 +11,19 @@ class Y2024Day1(Solution):
             [a, b] = line.split("   ")
             first.append(int(a))
             second.append(int(b))
+        first.sort()
+        second.sort()
         return first, second
 
     def solve_one(self, data: Tuple[List[int], List[int]]) -> Any:
-        first, second = data
-        first.sort()
-        second.sort()
         ans = 0
-        for a, b in zip(first, second):
+        for a, b in zip(data[0], data[1]):
             ans += abs(a - b)
         return ans
 
-    def solve_two(self, data: Any) -> Any:
-        print(data)
+    def solve_two(self, data: Tuple[List[int], List[int]]) -> Any:
+        second_frequency = Counter(data[1])
+        ans = 0
+        for a in data[0]:
+            ans += a * second_frequency.get(a, 0)
+        return ans
