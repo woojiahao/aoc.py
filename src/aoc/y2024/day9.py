7b3319771df92244ec91f64dd46cff49cb51e2d6
Jiahao, Woo <woojiahao1234@gmail.com>
2025-11-25 12:20:11 +0800

    Implement solution as DLL

--- src/aoc/y2024/day9.py
+++ src/aoc/y2024/day9.py
@@ -1,7 +1,62 @@
 from collections import deque
-from typing import Any, Deque, List, Tuple
+from typing import Any, Deque, List, Optional, Tuple
 
 from ..solution import Solution
+
+
+class FragmentNode:
+    def __init__(self, start: int, length: int) -> None:
+        self.start = start
+        self.length = length
+        self.prev: Optional["FragmentNode"] = None
+        self.next: Optional["FragmentNode"] = None
+
+    def __repr__(self) -> str:
+        return f"[{self.start}/{self.length}]"
+
+
+class FragmentList:
+    def __init__(self) -> None:
+        self.head = FragmentNode(-1, -1)
+        self.tail = FragmentNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def insert_end(self, node: FragmentNode) -> None:
+        tail_prev = self.tail.prev
+        assert tail_prev is not None
+        tail_prev.next = node
+        node.prev = tail_prev
+        self.tail.prev = node
+        node.next = self.tail
+
+    def pop_node(self, node: FragmentNode) -> FragmentNode:
+        node_prev = node.prev
+        node_next = node.next
+        assert node_prev is not None
+        assert node_next is not None
+        node_prev.next = node_next
+        node_next.prev = node_prev
+        node.next = None
+        node.prev = None
+        return node_prev
+
+    def insert_ahead(self, target: FragmentNode, node: FragmentNode) -> None:
+        target_next = target.next
+        assert target_next is not None
+        target.next = node
+        node.next = target_next
+        target_next.prev = node
+        node.prev = target
+
+    def __repr__(self) -> str:
+        cur = self.head.next
+        ll = []
+        while cur != self.tail:
+            assert cur is not None
+            ll.append(repr(cur))
+            cur = cur.next
+        return " -> ".join(ll)
 
 
 class Y2024Day9(Solution):
@@ -44,7 +99,7 @@ class Y2024Day9(Solution):
 
     def solve_two(self, data: List[int]) -> Any:
         # Behaves as a queue for front most empty slots
-        empty: Deque[Tuple[int, int]] = deque([])
+        empty: FragmentList = FragmentList()
 
         # Behaves as a stack for end most available slots
         filled: List[Tuple[int, int, int]] = []
@@ -54,27 +109,35 @@ class Y2024Day9(Solution):
         id = 0
         for d in data:
             if is_file:
-                filled.append((id, i, d))
+                if d != 0:
+                    filled.append((id, i, d))
                 id += 1
             else:
-                empty.append((i, d))
+                if d != 0:
+                    empty.insert_end(FragmentNode(i, d))
             i += d
             is_file = not is_file
+
         compacted_positions = []
         j = len(filled) - 1
         while j >= 0 and empty:
             last_block_id, last_block_start, last_block_length = filled.pop()
-            for k in range(len(empty)):
-                if empty[k][0] < last_block_start and empty[k][1] >= last_block_length:
-                    remaining = empty[k][1] - last_block_length
-                    empty_start = empty[k][0]
-                    del empty[k]
-                    if remaining > 0:
-                        empty.insert(k, (empty_start + last_block_length, remaining))
+            cur = empty.head.next
+            while cur != empty.tail:
+                assert cur is not None
+                cur_next = cur.next
+                if cur.start < last_block_start and cur.length >= last_block_length:
+                    remaining = cur.length - last_block_length
                     compacted_positions.append(
-                        (last_block_id, empty_start, last_block_length)
+                        (last_block_id, cur.start, last_block_length)
                     )
+                    if remaining == 0:
+                        empty.pop_node(cur)
+                    else:
+                        cur.start += last_block_length
+                        cur.length = remaining
                     break
+                cur = cur_next
             else:
                 compacted_positions.append(
                     (last_block_id, last_block_start, last_block_length)
