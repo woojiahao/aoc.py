ebd1d5924b19a38ab85074a21fc7fcdd81c6663a
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-01 13:14:41 +0800

    Solve day 1 2025

--- src/aoc/y2025/day1.py
+++ src/aoc/y2025/day1.py
@@ -1,4 +1,3 @@
-
 from typing import Any, List, Tuple
 
 from ..solution import Solution
@@ -6,12 +5,36 @@ from ..solution import Solution
 
 class Y2025Day1(Solution):
     def parse_data(self, contents: str) -> Any:
-        for line in contents.strip().split("\n"):
-            pass
-        return
+        rotations = [
+            int(rotation_raw[1:]) * (-1 if rotation_raw[0] == "L" else 1)
+            for rotation_raw in contents.strip().split("\n")
+        ]
+        return rotations
 
-    def solve_one(self, data: Any) -> Any:
-        pass
+    def solve_one(self, data: List[int]) -> Any:
+        pos = 50
+        dial_size = 100
+        ans = 0
+        for rotation in data:
+            pos = (pos + rotation) % dial_size
+            if pos == 0:
+                ans += 1
+        return ans
 
     def solve_two(self, data: Any) -> Any:
-        pass
+        pos = 50
+        dial_size = 100
+        ans = 0
+        # Rotate right past 0 -> number X -> number Y => X > Y
+        # Rotate left past 0 -> number X -> number Y => X < Y
+        # Pass 0 iff (pos + rotation) > 99 (pos + rotation) < 0
+        for rotation in data:
+            for _ in range(abs(rotation)):
+                if rotation < 0:
+                    pos = (pos - 1) % dial_size
+                else:
+                    pos = (pos + 1) % dial_size
+                if pos == 0:
+                    ans += 1
+
+        return ans
