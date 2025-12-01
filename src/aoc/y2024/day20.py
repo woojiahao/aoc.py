8a8a40eb137044df9d74eb1533c279a345165ad7
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-01 12:14:49 +0800

    Add parsing for day 20 2024

--- src/aoc/y2024/day20.py
+++ src/aoc/y2024/day20.py
@@ -1,4 +1,3 @@
-
 from typing import Any, List, Tuple
 
 from ..solution import Solution
@@ -6,9 +5,25 @@ from ..solution import Solution
 
 class Y2024Day20(Solution):
     def parse_data(self, contents: str) -> Any:
+        grid = []
+        start_r, start_c = -1, -1
+        end_r, end_c = -1, -1
         for line in contents.strip().split("\n"):
-            pass
-        return
+            row = []
+            r = 0
+            for c, cell in enumerate(line):
+                if cell == "S":
+                    start_r, start_c = r, c
+                elif cell == "E":
+                    end_r, end_c = r, c
+                elif cell == "#":
+                    row.append("#")
+                    continue
+                row.append(".")
+            r += 1
+            grid.append(row)
+
+        return grid, (start_r, start_c), (end_r, end_c)
 
     def solve_one(self, data: Any) -> Any:
         pass
