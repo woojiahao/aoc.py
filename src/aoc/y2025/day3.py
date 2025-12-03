7453108aebaf4a660955d3bc73204eca48d672a9
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-03 14:01:57 +0800

    Optimize day 3 2025 to solve both parts

--- src/aoc/y2025/day3.py
+++ src/aoc/y2025/day3.py
@@ -11,48 +11,32 @@ class Y2025Day3(Solution):
         return banks
 
     def solve_one(self, data: List[List[int]]) -> Any:
-        # ans = 0
-        # for bank in data:
-        #     n = len(bank)
-        #     max_joltage = 0
-        #     for i in range(n):
-        #         for j in range(i + 1, n):
-        #             max_joltage = max(max_joltage, bank[i] * 10 + bank[j])
-        #     ans += max_joltage
-        # return ans
-        #
-        ans = 0
-        for bank_ in data:
-            ans += self.__take__(bank_, 0, 0, 2, 0, {})
-        return ans
+        return self.__solve__(data, 2)
 
     def solve_two(self, data: List[List[int]]) -> Any:
+        return self.__solve__(data, 12)
+
+    def __solve__(self, data: List[List[int]], max_taken: int) -> int:
         ans = 0
         for bank_ in data:
-            ans += self.__take__(bank_, 0, 0, 12, 0, {})
+            ans += self.__take_batteries__(bank_, max_taken)
         return ans
 
-    def __take__(
-        self,
-        bank: List[int],
-        i: int,
-        taken: int,
-        max_taken: int,
-        acc: int,
-        memo: Dict[Tuple[int, int, int], int],
-    ) -> int:
-        key = (i, taken, acc)
-        if key in memo:
-            return memo[key]
-
-        if i >= len(bank) and taken < max_taken:
-            return 0
-
-        if i >= len(bank) or taken >= max_taken:
-            return acc
-
-        memo[key] = max(
-            self.__take__(bank, i + 1, taken + 1, max_taken, acc * 10 + bank[i], memo),
-            self.__take__(bank, i + 1, taken, max_taken, acc, memo),
-        )
-        return memo[key]
+    def __take_batteries__(self, bank: List[int], max_taken: int) -> int:
+        taken = 0
+        n = len(bank)
+        l, r = 0, n - 1
+        joltage = 0
+        while taken < max_taken:
+            remaining = max_taken - taken - 1
+            max_seen = bank[l]
+            max_seen_i = l
+            for i in range(l, r + 1):
+                remaining_digits = n - i - 1
+                if bank[i] > max_seen and remaining_digits >= remaining:
+                    max_seen = bank[i]
+                    max_seen_i = i
+            joltage = joltage * 10 + max_seen
+            l = max_seen_i + 1
+            taken += 1
+        return joltage
