de31582ab442c04de9ef66d3a9cdefd1cb8b51d4
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-05 13:11:45 +0800

    Solve day 5 2025

--- src/aoc/y2025/day5.py
+++ src/aoc/y2025/day5.py
@@ -1,4 +1,3 @@
-
 from typing import Any, List, Tuple
 
 from ..solution import Solution
@@ -6,12 +5,37 @@ from ..solution import Solution
 
 class Y2025Day5(Solution):
     def parse_data(self, contents: str) -> Any:
-        for line in contents.strip().split("\n"):
-            pass
-        return
+        [ingredient_ranges_raw, ingredients_raw] = contents.strip().split("\n\n")
+        ingredient_ranges = []
+        for r in ingredient_ranges_raw.split("\n"):
+            s, e = r.split("-")
+            ingredient_ranges.append([int(s), int(e)])
 
-    def solve_one(self, data: Any) -> Any:
-        pass
+        ingredient_ranges.sort()
+        merged_intervals = [ingredient_ranges[0]]
+        for i in range(1, len(ingredient_ranges)):
+            cur_range = ingredient_ranges[i]
+            if cur_range[0] <= merged_intervals[-1][1]:
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], cur_range[1])
+            else:
+                merged_intervals.append(cur_range)
 
-    def solve_two(self, data: Any) -> Any:
-        pass
+        ingredients = [int(l) for l in ingredients_raw.strip().split("\n")]
+        return merged_intervals, ingredients
+
+    def solve_one(self, data: Tuple[List[List[int]], List[int]]) -> Any:
+        ingredient_ranges, ingredients = data
+        ans = 0
+        for ingredient in ingredients:
+            for s, e in ingredient_ranges:
+                if s <= ingredient <= e:
+                    print(s, ingredient, e)
+                    ans += 1
+        return ans
+
+    def solve_two(self, data: Tuple[List[List[int]], List[int]]) -> Any:
+        ingredient_ranges, _ = data
+        ans = 0
+        for s, e in ingredient_ranges:
+            ans += e - s + 1
+        return ans
